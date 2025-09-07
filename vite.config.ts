6106399972da8791d804c2deae026cf3e28af79a
LSH <58622039+superstarMann@users.noreply.github.com>
2025-09-07 13:46:02 +0200

    ㅇ

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,17 @@
-import { defineConfig } from "vite";
+import { defineConfig, ConfigEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(({ mode }: ConfigEnv) => ({
+  base: mode === "production" ? "/fancake/" : "/",   // 👈 GitHub Pages 대응
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
